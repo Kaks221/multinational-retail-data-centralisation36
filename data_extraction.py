41876f3ef7ec8d9b2ab45ce94183a3a4ad7ca751
Kwaku Brobbey <kwaku.k.a.brobbey@gmail.com>
2024-01-06 18:32:57 +0000

    adding initial files from previous folder

---
+++ data_extraction.py
@@ -0,0 +1,14 @@
+class DataExtractor:
+    def __init__(self):
+        pass
+
+    def extract_csv_data(self, file_path):
+        pass
+
+    def extract_api_data(self_api_url)
+        pass
+
+    def extract_s3_data(self, s3_url):
+        pass
+
+    
---
+++ database_utils.py
@@ -0,0 +1,16 @@
+import yaml
+
+class DatabseConnector:
+    def __init__(self,connection_parameters):
+        self.connection_parameters = connection_parameters
+        pass
+    
+    def connect(self):
+        pass
+
+    def upload_data(self, data):
+        pass
+
+    def read_db_creds(file_path):
+
+
